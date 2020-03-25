d28b90dca23361d2c2353810682cf447ae5b6eb8
fahmichibani <fahmichibani@gmail.com>
2020-03-25 04:42:47 +0100

    props ans staus

---
+++ App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import Search from './src/Components/Search'
+
+export default class App extends React.Component {
+  render(){
+  return ( 
+       <Search/>
+  );
+ }
+}
+const styles=StyleSheet.create({
+  
+});
