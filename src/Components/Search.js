e90a152073bb8e2bad9665fdfdc08641e04ee5bf
fahmichibani <fahmichibani@gmail.com>
2020-03-25 05:18:18 +0100

    2

--- src/Components/Search.js
+++ src/Components/Search.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { StyleSheet, View, TextInput, Button, Text,FlatList } from 'react-native'
+import { StyleSheet, View, TextInput, Button, ActivityIndicator,FlatList } from 'react-native'
 import films from '../Helpers/filmsData'
 import FilmItem from './FilmItem'
 import { getFilmsFromApiWithSearchedText } from '../API/TMDBApi';
@@ -9,41 +9,61 @@ import { getFilmsFromApiWithSearchedText } from '../API/TMDBApi';
         constructor(props) {
           super(props)
           this.searchedText = "" // Initialisation de notre donnée searchedText en dehors du state
-          this.state = { films: [] }  
-               
+          this.state = { films: [] }  ,
+          this.isLoading= false // Par défaut à false car il n'y a pas de chargement tant qu'on ne lance pas de recherche
+  
         }
  
   _loadFilms() {
-    console.log(this.searchedText) // Un log pour vérifier qu'on a bien le texte du TextInput
-    if (this.searchedText.length > 0) { // Seulement si le texte recherché n'est pas vide
-      getFilmsFromApiWithSearchedText(this.searchedText).then(data => {
+    console.log(this.searchedText)                     // Un log pour vérifier qu'on a bien le texte du TextInput
+    if (this.searchedText.length > 0) {                 // Seulement si le texte recherché n'est pas vide
+       this.setState({ isLoading: true }) // Lancement du chargement 
+    getFilmsFromApiWithSearchedText(this.searchedText).then(data => {
          //  console.log(data), this.setState({ films: data.results })
-          this.setState({ films: data.results })
+          this.setState({
+             films: data.results,
+             isLoading: false // Arrêt du chargement
+            })
       })
     }
   }
     _searchTextInputChanged(text) {
           this.searchedText= text  
     }
-    render() {
-          console.log("RENDER")
-          return (
-            <View style={styles.main_container}>
-              <TextInput
-              style={styles.textinput}
-              placeholder='Titre du film'
-              onChangeText={(text) => this._searchTextInputChanged(text)}
-            />
-              <Button title='Rechercher' onPress={() => this._loadFilms()}/>
-      
-              <FlatList
-                data={this.state.films}
-                keyExtractor={(item) => item.id.toString()}
-                renderItem={({item}) => <FilmItem film={item}/>}
-                />             
-            </View>
-          )
-        }
+    _displayLoading() {
+      if (this.state.isLoading) {
+        return (
+          <View style={styles.loading_container}>
+            <ActivityIndicator size='large' />
+            {/* Le component ActivityIndicator possède une propriété size pour définir la taille du visuel de chargement : small ou large. Par défaut size vaut small, on met donc large pour que le chargement soit bien visible */}
+          </View>
+        )
+      }
+    }
+
+ 
+      render() {
+        console.log("RENDER:")
+        console.log(this.state.isLoading)
+       // Components/Search.js  
+       return (
+        <View style={styles.main_container}>
+          <TextInput
+           style={styles.textinput}
+            placeholder='Titre du film'
+            onChangeText={(text) => this._searchTextInputChanged(text)}
+            onSubmitEditing={() => this._loadFilms()}
+        />
+        <Button title='Rechercher' onPress={() => this._loadFilms()}/>
+        <FlatList
+          data={this.state.films}
+          keyExtractor={(item) => item.id.toString()}
+          renderItem={({item}) => <FilmItem film={item}/>}
+        />
+        {this._displayLoading()}
+      </View>
+    )
+  }
       }
       
       const styles = StyleSheet.create({
@@ -58,6 +78,15 @@ import { getFilmsFromApiWithSearchedText } from '../API/TMDBApi';
           borderColor: '#000000',
           borderWidth: 1,
           paddingLeft: 5
+        },
+        loading_container: {
+          position: 'absolute',
+          left: 0,
+          right: 0,
+          top: 100,
+          bottom: 0,
+          alignItems: 'center',
+          justifyContent: 'center'
         }
       })
 
