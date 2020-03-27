088c4df7900d5fda53e3e25be807930be450295c
fahmichibani <fahmichibani@gmail.com>
2020-03-27 04:28:22 +0100

    navig 2

--- App.js
+++ App.js
@@ -4,21 +4,15 @@ import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 import Search from './Components/Search';
   
-function _Search() {
-  return (<Search/> );
-}
 const Stack = createStackNavigator();
-  
-export default class App extends React.Component {
-
-  render(){
+export default class App extends React.Component   {
+  render() {
     return (
       <NavigationContainer>
-        <Stack.Navigator initialRouteName="Searching">
-          <Stack.Screen 
-          name="Rechercher+" 
-          component={_Search}
-           />
+        <Stack.Navigator initialRouteName="Rechercher+">
+          <Stack.Screen name="Rechercher+"> 
+            {props => <Search/>}
+           </Stack.Screen>
          </Stack.Navigator>
       </NavigationContainer>
     );
