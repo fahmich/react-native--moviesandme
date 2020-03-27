ec2166f9f9aca6b8f33eed1342387c08b7a3f3cf
fahmichibani <fahmichibani@gmail.com>
2020-03-27 02:45:51 +0100

    navigation set up

--- App.js
+++ App.js
@@ -1,14 +1,28 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-import Search from './src/Components/Search'
-
-export default class App extends React.Component {
-  render(){
-  return ( 
-       <Search/>
-  );
- }
-}
-const styles=StyleSheet.create({
+import * as React from 'react';
+import { View, Text } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import Search from './Components/Search';
   
-});
+function _Search() {
+  return (<Search/> );
+}
+const Stack = createStackNavigator();
+  
+export default class App extends React.Component {
+
+  render(){
+    return (
+      <NavigationContainer>
+        <Stack.Navigator initialRouteName="Searching">
+          <Stack.Screen 
+          name="Rechercher+" 
+          component={_Search}
+           />
+         </Stack.Navigator>
+      </NavigationContainer>
+    );
+  }
+  }
+ 
+
