651a96c2588f20948708975364a7cc0596cc0d29
fahmichibani <fahmichibani@gmail.com>
2020-03-31 06:09:34 +0100

    fix tab bar

--- Navigation_Favorite.js
+++ Navigation_Favorite.js
@@ -4,6 +4,7 @@ import * as React from 'react';
  import Store from './Store/configureStore'
 import FilmList from './Components/FilmList';
 import Favorites from './Components/Favorites';
+import FilmDetail from './Components/FilmDetail ';
  
  const Stack = createStackNavigator();
  export default class Navigation_Favorite extends React.Component{
@@ -13,6 +14,8 @@ import Favorites from './Components/Favorites';
           <Provider store={Store}>
             <Stack.Navigator initialRouteName="Favorites**">
                <Stack.Screen name="Favorites" component={Favorites}/> 
+               <Stack.Screen name="FilmDetail" component={FilmDetail}/> 
+
             </Stack.Navigator>
            </Provider>
         );   
